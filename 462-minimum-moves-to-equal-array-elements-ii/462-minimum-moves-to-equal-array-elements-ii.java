76e49379f2284a216567dd40c0fd1e4aa4d25236
Amrit Pandey <25566630+ok-ape@users.noreply.github.com>
2022-06-30 14:21:17 +0530

    Time: 3 ms (98.32%), Space: 45.9 MB (42.98%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,17 +1,42 @@
 class Solution {
     public int minMoves2(int[] nums) {
         int count = 0;
-        Arrays.sort(nums);
-        int M = -1;
-        int n = nums.length;
-        if ((nums.length & 1) == 0) {
-            M = (nums[(n - 1) / 2] + nums[n / 2]) / 2;
-        } else {
-            M = nums[n / 2];
-        }
-        for (int t : nums) {
-            count += Math.abs(t - M);
+        int median = nums[partition(nums, 0, nums.length - 1)];
+        for (int n : nums) {
+            count += Math.abs(n - median);
         }
         return count;
+    }
+    
+    public int partition(int[] A, int l, int h) {
+        int i, j;
+        final int pivot, randomIdx;
+        randomIdx = new Random().nextInt(h - l + 1) + l;
+        swap(A, h, randomIdx);
+        pivot = A[h];
+        i = l - 1;
+        j = l;
+        while (j < h) {
+            if (A[j] <= pivot) {
+                i++;
+                swap(A, i, j);
+            }
+            j++;
+        }
+        i++;
+        swap(A, i, h);
+        if (i == A.length / 2) {
+            return i;
+        }
+        if (i < A.length / 2) {
+            return partition(A, i + 1, h);
+        }
+        return partition(A, l, i - 1);
+    }
+    
+    public void swap(int[] A, int x, int y) {
+        int tmp = A[x];
+        A[x] = A[y];
+        A[y] = tmp;
     }
 }
