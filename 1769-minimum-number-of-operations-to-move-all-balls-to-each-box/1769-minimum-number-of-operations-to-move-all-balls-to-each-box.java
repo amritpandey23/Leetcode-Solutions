06fe4b55d36955734d6650f93bf35e926ac64930
Amrit Pandey <25566630+ok-ape@users.noreply.github.com>
2022-08-02 13:39:15 +0530

    Time: 4 ms (86.83%), Space: 48.3 MB (13.62%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,21 +1,26 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int n;
-        int[] Ans;
-        TreeSet<Integer> set;
-        
+        int n, ballCount;
+        int[] leftWays, rightWays, Ans;
         n = boxes.length();
+        leftWays = new int[n];
+        rightWays = new int[n];
         Ans = new int[n];
-        set = new TreeSet<>();
-        for (int i = 0; i < n; ++i) {
-            if (boxes.charAt(i) == '1') {
-                set.add(i);
-            }
+
+        ballCount = boxes.charAt(0) == '1' ? 1 : 0;
+        leftWays[0] = 0;
+        for (int i = 1; i < n; ++i) {
+            leftWays[i] += leftWays[i - 1] + ballCount;
+            ballCount += boxes.charAt(i) == '1' ? 1 : 0;
+        }
+        ballCount = boxes.charAt(n - 1) == '1' ? 1 : 0;
+        rightWays[n - 1] = 0;
+        for (int i = n - 2; i >= 0; --i) {
+            rightWays[i] += rightWays[i + 1] + ballCount;
+            ballCount += boxes.charAt(i) == '1' ? 1 : 0;
         }
         for (int i = 0; i < n; ++i) {
-            for (int bi : set) {
-                Ans[i] += Math.abs(i - bi);
-            }
+            Ans[i] = leftWays[i] + rightWays[i];
         }
         return Ans;
     }
