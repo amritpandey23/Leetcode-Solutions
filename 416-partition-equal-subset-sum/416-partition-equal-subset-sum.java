090de24bf0273317f1f0961087d3a26a91a6ddf8
Amrit Pandey <25566630+ok-ape@users.noreply.github.com>
2022-08-26 18:46:55 +0530

    Time: 321 ms (8.21%), Space: 72.4 MB (14.79%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -8,22 +8,21 @@ class Solution {
             return false;
         }
         sum /= 2;
-        Boolean[][] dp = new Boolean[sum + 1][nums.length + 1];
-        return subsetSum(nums, sum, nums.length, dp);
-    }
-    
-    private boolean subsetSum(int[] nums, int sum, int n, Boolean[][] dp) {
-        if (n == 0 || sum < 0) {
-            return false;
+        Boolean[][] dp = new Boolean[nums.length + 1][sum + 1];
+        for (int i = 0; i <= nums.length; ++i) {
+            for (int j = 0; j <= sum; ++j) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0 || j == 0) {
+                    dp[i][j] = false;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                    if (nums[i - 1] <= j) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                    }
+                }
+            }
         }
-        if (sum == 0) {
-            return true;
-        }
-        if (dp[sum][n] != null) {
-            return dp[sum][n];
-        }
-        return dp[sum][n] = 
-            subsetSum(nums, sum - nums[n - 1], n - 1, dp) || 
-            subsetSum(nums, sum, n - 1, dp);
+        return dp[nums.length][sum];
     }
 }
